f48530be280d83527501ba07c10a5612bb48efda
kthordarson <kthordarson@gmail.com>
2013-10-06 00:39:16 +0000

    f52

--- sickbeard/providers/__init__.py
+++ sickbeard/providers/__init__.py
@@ -26,7 +26,8 @@ __all__ = ['ezrss',
            'scc',
            'hdbits',
            'iptorrents',
-           'omgwtfnzbs'
+           'omgwtfnzbs',
+           'deildu'
            ]
 
 import sickbeard
