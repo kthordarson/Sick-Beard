c11fe06ae59bdd5a2aaaef994978a3e78432c0da
kthordarson <kthordarson@gmail.com>
2013-10-09 11:55:47 +0000

    deildu small regexfix

--- sickbeard/common.py
+++ sickbeard/common.py
@@ -20,6 +20,7 @@ import os.path
 import operator
 import platform
 import re
+import logger
 
 from sickbeard import version
 
@@ -168,28 +169,6 @@ class Quality:
             if regex_match:
                 return x
         
-#        checkName = lambda list, func: func([re.search(x, name, re.I) for x in list])
-#
-#        if checkName(["(pdtv|hdtv|dsr|tvrip|web.dl|webrip).(xvid|x264|h.?264)"], all) and not checkName(["(720|1080)[pi]"], all):
-#            return Quality.SDTV
-#        elif checkName(["(dvdrip|bdrip)(.ws)?.(xvid|divx|x264)"], any) and not checkName(["(720|1080)[pi]"], all):
-#            return Quality.SDDVD
-#        elif checkName(["720p", "hdtv", "x264"], all) or checkName(["hr.ws.pdtv.x264"], any) and not checkName(["(1080)[pi]"], all):          
-#            return Quality.HDTV                                                                        
-#        elif checkName(["720p|1080i", "hdtv", "mpeg2"], all):
-#            return Quality.RAWHDTV                                                                     
-#        elif checkName(["1080p", "hdtv", "x264"], all):         
-#            return Quality.FULLHDTV                                                                    
-#        elif checkName(["720p", "web.dl|webrip", "h.?264"], all) or checkName(["720p", "itunes", "h.?264"], all):
-#            return Quality.HDWEBDL                                                                     
-#        elif checkName(["1080p", "web.dl|webrip", "h.?264"], all) or checkName(["1080p", "itunes", "h.?264"], all):     
-#            return Quality.FULLHDWEBDL                                                                 
-#        elif checkName(["720p", "bluray|hddvd", "x264"], all):
-#            return Quality.HDBLURAY                                                                    
-#        elif checkName(["1080p", "bluray|hddvd", "x264"], all):
-#            return Quality.FULLHDBLURAY
-#        else:
-#            return Quality.UNKNOWN
 
     @staticmethod
     def sceneQuality(name):
@@ -198,30 +177,41 @@ class Quality:
         """
 
         name = os.path.basename(name)
-
+        logger.log(u"common.py checking: "+name, logger.DEBUG)
         checkName = lambda list, func: func([re.search(x, name, re.I) for x in list])
 
-        if checkName(["(pdtv|hdtv|dsr|tvrip|web.dl|webrip).(xvid|x264|h.?264)"], all) and not checkName(["(720|1080)[pi]"], all):
+        if checkName(["(pdtv|hdtv|dsr|tvrip|web.dl|webrip|WEB-DL|web-d|WEB-D).(xvid|x264|h.?264|AAC|x26)"], all) and not checkName(["(720|1080)[pi]"], all):
+            logger.log(u"common.py match sdtv: "+name, logger.DEBUG)
             return Quality.SDTV
         elif checkName(["(dvdrip|b[r|d]rip)(.ws)?.(xvid|divx|x264)"], any) and not checkName(["(720|1080)[pi]"], all):
+            logger.log(u"common.py match sddvd: "+name, logger.DEBUG)
             return Quality.SDDVD
         elif checkName(["720p", "hdtv", "x264"], all) or checkName(["hr.ws.pdtv.x264"], any) and not checkName(["(1080)[pi]"], all):          
+            logger.log(u"common.py match hdtv: "+name, logger.DEBUG)
             return Quality.HDTV                                                                        
         elif checkName(["720p|1080i", "hdtv", "mpeg-?2"], all):
+            logger.log(u"common.py match rawhdtv: "+name, logger.DEBUG)
             return Quality.RAWHDTV                                                                     
         elif checkName(["1080p", "hdtv", "x264"], all):         
+            logger.log(u"common.py match fullhdtv: "+name, logger.DEBUG) 
             return Quality.FULLHDTV                                                                    
         elif checkName(["720p", "web.dl", "h.?264"], all) or checkName(["720p", "itunes", "h.?264"], all):
+            logger.log(u"common.py match hdwebdl: "+name, logger.DEBUG)
             return Quality.HDWEBDL                                                                     
         elif checkName(["1080p", "web.dl", "h.?264"], all) or checkName(["1080p", "itunes", "h.?264"], all):     
+            logger.log(u"common.py match fullhdwebdl: "+name, logger.DEBUG)
             return Quality.FULLHDWEBDL                                                                 
         elif checkName(["720p", "webrip", "x264"], all):
+            logger.log(u"common.py match hdwebdl: "+name, logger.DEBUG)
             return Quality.HDWEBDL                                                                     
         elif checkName(["1080p", "webrip", "x264"], all):
+            logger.log(u"common.py match fullhdwebdl: "+name, logger.DEBUG)
             return Quality.FULLHDWEBDL        
         elif checkName(["720p", "bluray|hddvd|b[r|d]rip", "x264"], all):
+            logger.log(u"common.py match hdbluray: "+name, logger.DEBUG)
             return Quality.HDBLURAY                                                                    
         elif checkName(["1080p", "bluray|hddvd|b[r|d]rip", "x264"], all):
+            logger.log(u"common.py match fullhdbluray: "+name, logger.DEBUG)
             return Quality.FULLHDBLURAY
         else:
             return Quality.UNKNOWN
